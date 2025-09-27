27db43132daa8b7219646da73dbaf1a8646598db
Anthony Gargiulo <code@agarg.me>
2025-09-27 01:26:28 -0700

    update aws provider version

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "registry.terraform.io/hashicorp/aws"
-      version = "~> 5"
+      version = "~> 6"
 
       configuration_aliases = [aws.ci, aws.route53]
     }
